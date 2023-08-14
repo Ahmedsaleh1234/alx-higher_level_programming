4bcc44fcc37ba05e3473376827d0fe858ef12d22
Ahmedsaleh1234 <a12661826@gmail.com>
2023-08-14 11:12:21 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    string = ""
+    "removes all characters c and C from a string"
+
+    "new string to return"
+    no_c_string = ""
     for i in range(len(my_string)):
-        if my_string[i] != "c" and my_string[i] != "C":
-            string += my_string[i]
-    return (string)
+        "append if the char is not 'c' or 'C'"
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            no_c_string += my_string[i]
+    return (no_c_string)
