e73dcde0e91c6471f85ef3567e81867c53a71221
Ahmedsaleh1234 <a12661826@gmail.com>
2023-08-16 15:49:10 -0700

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,5 +3,5 @@ def best_score(a_dictionary):
     if a_dictionary:
         a = list(a_dictionary.keys())
         return max(a)
-    else:
+    elif not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
