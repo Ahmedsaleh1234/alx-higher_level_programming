788231c079dcd8f39cde6a15bc2f29165248c87e
Ahmedsaleh1234 <a12661826@gmail.com>
2023-10-18 16:45:30 -0700

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- Create database an user with read permissions
--- Execute: cat 2-create_read_user.sql | mysql -hlocalhost -uroot -p
+
 create database if not exists hbtn_0d_2;
 create user if not exists user_0d_2@localhost identified by 'user_0d_2_pwd';
 grant select on hbtn_0d_2 . * to user_0d_2@localhost;
