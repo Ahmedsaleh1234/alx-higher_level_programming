4e694c12653e321e617398d88be73e75a6179e92
Ahmedsaleh1234 <a12661826@gmail.com>
2023-10-18 22:44:47 +0000

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- a script that lists all privileges of the MySQL
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2
  
 show grants for user_0d_1@localhost;
 show grants for user_0d_2@localhost;
