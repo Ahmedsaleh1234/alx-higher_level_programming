9ca302d660cb1e4330e5a597ec9872f9fb867ead
Ahmedsaleh1234 <a12661826@gmail.com>
2023-10-19 03:41:10 -0700

    inter join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- script that lists all cities contained in the database
 
 select cities.id, cities.name, states.name from cities
-join states on cities.state_id = states.id
+join states on cities.state_id=states.id
 order by cities.id;
