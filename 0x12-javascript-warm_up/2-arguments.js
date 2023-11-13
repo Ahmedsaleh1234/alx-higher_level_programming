25fa5008e964933c3e63d20632107f3c405d3305
Ahmedsaleh1234 <a12661826@gmail.com>
2023-11-13 09:31:10 -0800

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 /* script that prints a message depending
 of the number of arguments passed */
-if (process.argv.length < 3) {
+const num = process.argv.length;
+if (num < 3) {
   console.log('No argument');
 } else {
   console.log('Arguments found');
