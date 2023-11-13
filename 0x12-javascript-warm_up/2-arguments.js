def5ce81e32e7a8da2c015997f6e53ec60be40db
Ahmedsaleh1234 <a12661826@gmail.com>
2023-11-13 09:34:17 -0800

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,6 +4,8 @@ of the number of arguments passed */
 const num = process.argv.length;
 if (num < 3) {
   console.log('No argument');
+} else if (num === 3) {
+  console.log('Arguments found');
 } else {
   console.log('Arguments found');
 }
