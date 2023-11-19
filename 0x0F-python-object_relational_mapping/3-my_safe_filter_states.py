086488626c377b8c8f0a0b48acc950efdc25d34e
Ahmedsaleh1234 <a12661826@gmail.com>
2023-11-19 10:16:56 -0800

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""that takes in an argument and displays all values"""
+"""Lists states"""
+
 import MySQLdb
 from sys import argv
-if __name__ == '__main__':
-    con = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                          passwd=argv[2], db=argv[3])
-    ch = con.cursor()
-    ch.execute("SELECT * FROM states WHERE BINARY name = %s", [argv[4]])
-    rows = ch.fetchall()
-    for i in rows:
-        print(i)
-    ch.close()
-    con.close()
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
+                (argv[4], ))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
