7dc6dc02f1db5237bb4e482f9c9399c8af43569c
Ahmedsaleh1234 <a12661826@gmail.com>
2023-11-19 10:34:16 -0800

    my_safe_filter_states

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
-"""Displays all values in save mode"""
+"""Script that takes in an argument and displays all values"""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
+    # Connect to the database
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    # Create a cursor object
+    cur = db.cursor()
+    # Prepare a parameterized query
+    query = "SELECT * FROM states WHERE name = %s"
+    # Execute the query with the user input as the parameter
+    cur.execute(query, (sys.argv[4],))
+    # Fetch all the results
+    rows = cur.fetchall()
+    # Print each row
+    for row in rows:
+        print(row)
+    # Close the cursor
+    cur.close()
+    # Close the database connection
+    db.close()
