2b9373c483ffe1f692369b2c7b562c1a6e0d3db7
Ahmedsaleh1234 <a12661826@gmail.com>
2024-01-03 08:33:31 -0800

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,5 @@
 const request = require('request');
 
 request(process.argv[2], function (_err, req) {
-  console.log('code: ', req.statusCode);
+  console.log('code: ', req.statusCode); // Print the response status code if a response was received
 });
