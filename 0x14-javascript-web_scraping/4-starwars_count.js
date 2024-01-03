9b65cb5048dca5152c2eee10453eb52f91485073
Ahmedsaleh1234 <a12661826@gmail.com>
2024-01-03 10:11:59 -0800

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+let count = 0;
+request(process.argv[2], function (err, req, body) {
+  if (!err) {
+    body = JSON.parse(body).results;
+    for (let i = 0; i < body.length; i++) {
+      const cs = body[i].characters;
+      for (let j = 0; j < cs.length; j++) {
+        const c = cs[j];
+        const cid = c.split('/')[5];
+        if (cid === '18') {
+          count += 1;
+        }
+      }
+    }
+  }
+  console.log(count);
+});
